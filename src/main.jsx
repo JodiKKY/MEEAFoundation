bcef9181ce33793afcc7fda14501d30305ecb114
JodiKKY <jodiyirenkyi@gmail.com>
2025-10-30 14:27:41 +0000

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './Layout/layout.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import favicon from './assets/favicon.svg'; 
+
+
+const setFavicon = (image) => {
+  let link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.href = image;
+};
+
+setFavicon(favicon); 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>    
+        <Layout>
+          <App />
+        </Layout>
+    </BrowserRouter>
+  </React.StrictMode>
+);
